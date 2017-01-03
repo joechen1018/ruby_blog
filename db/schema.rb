09d7119bfefc90ed09ba07200ec1dbb908f46c55
Joe Chen <joe.9x9@gmail.com>
2017-01-03 15:58:56 +0800

    set up relations for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102055746) do
+ActiveRecord::Schema.define(version: 20170103075358) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170102055746) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
