91c3748e09e143b9c1c0cce400b9a7a29fab22c2
Joe Chen <joe.9x9@gmail.com>
2017-01-09 14:56:31 +0800

    #switched to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20170103075358) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "adminpack"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
@@ -27,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170103075358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,8 +47,8 @@ ActiveRecord::Schema.define(version: 20170103075358) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
